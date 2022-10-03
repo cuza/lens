d4209eeca1a076a13daa235d675f6ab81ae36389
Sebastian Malton <sebastian@malton.name>
2022-10-03 11:41:59 -0300

    Remove mac-ca usage since it was only in tests (#6043)
    * Make injecting CAs injectable, remove mac-ca as dependency
    * Fix win-ca failing on electron renderer on windows
    * Fix the matcher under features/ for main
    
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/common/fs/exec-file.injectable.ts
+++ src/common/fs/exec-file.injectable.ts
@@ -8,30 +8,50 @@ import { execFile } from "child_process";
 import type { AsyncResult } from "../utils/async-result";
 
 export interface ExecFile {
+  (filePath: string): Promise<AsyncResult<string, { stderr: string; error: Error }>>;
+  (filePath: string, argsOrOptions: string[] | ExecFileOptions): Promise<AsyncResult<string, { stderr: string; error: Error }>>;
   (filePath: string, args: string[], options: ExecFileOptions): Promise<AsyncResult<string, { stderr: string; error: Error }>>;
 }
 
 const execFileInjectable = getInjectable({
   id: "exec-file",
 
-  instantiate: (): ExecFile => (filePath, args, options) => new Promise((resolve) => {
-    execFile(filePath, args, options, (error, stdout, stderr) => {
-      if (error) {
-        resolve({
-          callWasSuccessful: false,
-          error: {
-            error,
-            stderr,
-          },
+  instantiate: (): ExecFile => {
+    return (filePath: string, argsOrOptions?: string[] | ExecFileOptions, maybeOptions?: ExecFileOptions) => {
+      const { args, options } = (() => {
+        if (Array.isArray(argsOrOptions)) {
+          return {
+            args: argsOrOptions,
+            options: maybeOptions ?? {},
+          };
+        } else {
+          return {
+            args: [],
+            options: argsOrOptions ?? {},
+          };
+        }
+      })();
+
+      return new Promise((resolve) => {
+        execFile(filePath, args, options, (error, stdout, stderr) => {
+          if (error) {
+            resolve({
+              callWasSuccessful: false,
+              error: {
+                error,
+                stderr,
+              },
+            });
+          } else {
+            resolve({
+              callWasSuccessful: true,
+              response: stdout,
+            });
+          }
         });
-      } else {
-        resolve({
-          callWasSuccessful: true,
-          response: stdout,
-        });
-      }
-    });
-  }),
+      });
+    };
+  },
 
   causesSideEffects: true,
 });
